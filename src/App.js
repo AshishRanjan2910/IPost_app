a06ccd12e88129d95fa5d3f0539884d4b09e73f6
AshishRanjan2910 <67757899+AshishRanjan2910@users.noreply.github.com>
2021-06-28 10:38:31 +0530

    pages & navbar added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './/Normalize.css'
 import './Style.css';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+import Blogs from './Pages/Blogs';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div>
+      <Navbar/>
+      <Home/>
+      <Projects/>
+      <Blogs/>
+      <Contact/>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="Navbar">
       <h3 className="Brand">Ranjan</h3>
       <ul className="Nav-links">
-        <Link to="/" className="Nav-item">
-          Home
-        </Link>
-        <Link to="/Projects" className="Nav-item">
-          Projects
-        </Link>
-        <Link to="/Blogs" className="Nav-item">
-          Blogs
-        </Link>
-        <Link to="/Contact" className="Nav-item">
-          Contact
-        </Link>
+        <li className = "Nav-item">Home</li>
+        <li className = "Nav-item">Projects</li>
+        <li className = "Nav-item">Blogs</li>
+        <li className = "Nav-item">Contacts</li>
       </ul>
     </nav>
   );
