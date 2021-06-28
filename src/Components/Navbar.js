33e54575ec28f45d2e0901212089e669098f7c4e
AshishRanjan2910 <67757899+AshishRanjan2910@users.noreply.github.com>
2021-06-28 09:40:45 +0530

    Necessary components & pages added.

---
+++ src/Components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <h3 className="Brand">Ranjan</h3>
+      <ul className="Nav-links">
+        <Link to="/" className="Nav-item">
+          Home
+        </Link>
+        <Link to="/Projects" className="Nav-item">
+          Projects
+        </Link>
+        <Link to="/Blogs" className="Nav-item">
+          Blogs
+        </Link>
+        <Link to="/Contact" className="Nav-item">
+          Contact
+        </Link>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
